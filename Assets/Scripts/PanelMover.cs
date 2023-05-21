b86119960c9031e30cf2c8f30e9e031a5df66c7e
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2023-05-21 18:55:51 +0900

    fixed menu animation

--- Assets/Scripts/PanelMover.cs
+++ Assets/Scripts/PanelMover.cs
@@ -1,23 +1,55 @@
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class PanelMover : MonoBehaviour
 {
-    [SerializeField] private Animator _target;
+    [SerializeField] private GameObject _target;
+    [SerializeField] private GameObject _hidePlace;
+    [SerializeField] private float _moveSpeed;
 
     private bool _isShow = true;
+    private Vector3 _startPoint;
+    private CancellationTokenSource _cancellationTokenSource;
 
-    public void OnShowHide()
+    private void OnDisable()
     {
-        if(_isShow == true)
+        _cancellationTokenSource?.Cancel();
+    }
+
+    private void Start()
+    {
+        _startPoint = _target.transform.position;
+    }
+
+    public async void OnShowHide()
+    {
+        _cancellationTokenSource?.Cancel();
+        _cancellationTokenSource = new CancellationTokenSource();
+
+        if (_isShow == true)
         {
-            _target.SetTrigger(AnimationsController.Params.Hide);
+            await MoveAsync(_hidePlace.transform.position, _cancellationTokenSource.Token);
             _isShow = false;
         }
         else
         {
-            _target.SetTrigger(AnimationsController.Params.Show);
+            await MoveAsync(_startPoint, _cancellationTokenSource.Token);
             _isShow = true;
+        }
+    }
+
+    private async Task MoveAsync(Vector3 targetPositioon, CancellationToken token)
+    {
+        while(_target.transform.position.ToString() != targetPositioon.ToString())
+        {
+            if (token.IsCancellationRequested)
+            {
+                break;
+            }
+
+            _target.transform.position = Vector3.MoveTowards(_target.transform.position, targetPositioon, _moveSpeed * Time.deltaTime);
+            await Task.Yield();
         }
     }
 }
